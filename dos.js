5128a8c72ef73f191060b04d6dd69273e7a3e4fb
lazo92 <jlazo912@gmail.com>
2021-08-11 11:36:32 -0300

    entrega recuperatorio

--- dos.js
+++ dos.js
@@ -1,3 +1,140 @@
 function mostrar() {
-  alert("Ejercicio 2");
+
+  let carrera;
+  let sexo;
+  let cantidadMaterias // 1 - 5 
+  let notaPromedio; // 0 - 10
+  let edad; //Se debe informar de existir, o informar que no existe , en el caso que corresponda.
+  //A
+  let nombreMjFisica;
+  let mejorpromFisica;
+  let flagMjFis=1
+  //B
+  let nomAlumMasjov;
+  let EdadAlumMasJov;
+  let flagJov=1
+  //C 
+  let contquimica=0;
+  let contFisica=0;
+  let contSistemas=0;
+  let porcQuimica=0;
+  let porcFisica=0;
+  let porcSistemas=0;
+  //D
+  let edadMasMaterias;
+  let nombreMasMaterias;
+  let masMaterias;
+  let flagMaterias =1;
+
+
+
+   //500
+  for(let i=0;i < 2; i++){
+
+    nombre=prompt("ingrese nombre");
+
+   carrera=prompt("ingrese carrera: quimica / fisica / sistemas").toLowerCase();
+    while(! (carrera=="quimica" || carrera =="fisica" || carrera== "sistemas" )){
+     carrera=prompt("error. ingrese carrera: quimica / fisica / sistemas").toLowerCase();
+    }
+
+    sexo=prompt("ingrese sexo: masculino / femenino / no binario").toLowerCase();
+    while(! (sexo=="masculino" || sexo =="femenino" || sexo== "no binario" )){
+     sexo=prompt("error. ingrese sexo: masculino / femenino / no binario").toLowerCase();
+    }
+
+
+    cantidadMaterias=parseInt(prompt("ingrese cantidad de  materias de 1 a 5: "));
+    while(!(cantidadMaterias>= 1 && cantidadMaterias <= 5)){
+      cantidadMaterias=parseInt(prompt("fuera de rango. ingrese materias de 1 a 5:"));
+    }
+
+
+    nota=parseInt(prompt("ingrese nota: "));
+    while(!(nota>=0 && nota<=10)){
+      nota=parseInt(prompt("error. ingrese nota"));
+    }
+
+
+    edad=parseInt(prompt("ingrese edad: "));
+    while(isNaN(edad)|| edad < 1){
+      alert("edad no existente")
+      edad=parseInt(prompt("error. ingrese edad"));
+    }alert("edad existente")
+
+
+
+    if(carrera=="fisica"){ // A
+
+      contFisica++;
+
+      if(flagMjFis || notaPromedio > mejorpromFisica){
+        mejorpromFisica = notaPromedio;
+        nombreMjFisica = nombre
+        flagMjFis=0;
+      }
+
+
+
+    }else if(carrera == "quimica"){
+      contquimica++;
+
+
+    }else{
+
+      contSistemas++;
+
+    }
+
+
+
+    if(sexo=="femenino"){ //B
+      if( flagJov|| edad < EdadAlumMasJov){
+        EdadAlumMasJov= edad;
+        nomAlumMasjov =nombre;
+        flagJov=0;
+      }
+    }
+
+
+    if (!( carrera== "quimica")){
+      if(flagMaterias || cantidadMaterias > masMaterias){
+        masMaterias = cantidadMaterias;
+        edadMasMaterias = edad;
+        nombreMasMaterias = nombre;
+        flagMaterias=0;
+
+
+      }
+    }
+
+
+  }// fin de for
+
+
+  //c
+  porcQuimica= contquimica / .2;
+  porcFisica = contFisica / .2;
+  porcSistemas= contSistemas / .2;
+ 
+
+  //////////////////////////////
+  document.write("A) Nombre mejor promedio: " + nombreMjFisica+ "<br>");
+  document.write("B) el nombre de la alumna mas joven es: " + nomAlumMasjov+ "<br>");
+  document.write("C) el porcentaje de estudiantes  en cada materia es: quimica = "+ porcQuimica + " fisica = " + porcFisica + " Sistemas = " + porcSistemas + "<br>");
+  document.write("D) el estudiante con mas materias cursadas es: " + nombreMasMaterias + " y tiene " + edadMasMaterias + " años <br>");
+ 
+
+
+
+
+  
+
+
+
+
+
+
+
+
 }
--- uno.js
+++ uno.js
@@ -1,3 +1,115 @@
 function mostrar() {
-  alert("Ejercicio 1");
+
+  let nombre; //entre 3 y 6 caracteres 
+  let tipo; // mamifero, ave, reptil
+  let sangre; // caliente o fria 
+  let edad; // >0
+  let sexo // m macho , h hembra
+  let respuesta;
+  let promEdadRep=0;
+  let acumEdadRep=0
+  let contRep=0;
+  let tipoMasJov;
+  let sexoMasJov;
+  let edadMasJov;
+  let flagMasJov=1
+  let contAve=0;
+  let contMascotas =0;
+  let procentAves=0
+  let contmamifero=0;
+  let maxCantMascotas;
+
+
+
+
+  do{
+
+    nombre=prompt("ingrese nombre ");
+    while(nombre.length < 3 || nombre.length > 8){
+       nombre=prompt("nombre fuera de rango. ingrese nombre ");
+    }
+
+    tipo=prompt("ingrese tipo: mamifero / ave / reptil").toLowerCase();
+    while(!(tipo=="mamifero" || tipo =="ave" || tipo== "reptil" )){
+      tipo=prompt("error. ingrese tipo: mamifero / ave / reptil").toLowerCase();
+    }
+
+    if(tipo=="reptil"){
+      sangre= "fria";
+
+    }else{
+      sangre=prompt("ingrese sangre: fria o caliente").toLowerCase();
+      while(!(sangre == "fria" || sangre == "caliente")){
+        sangre=prompt("error. ingrese sangre: fria o caliente").toLowerCase();
+      }
+    }
+
+    edad=parseInt(prompt("ingrese edad: "));
+    while(isNaN(edad)|| edad < 0){
+      edad=parseInt(prompt("error. ingrese edad")); 
+    }
+
+    sexo= prompt("ingrese sexo: m / h").toLowerCase();
+    while(sexo!="m" && sexo!= "h"){
+      sexo= prompt("error. ingrese sexo: m / h").toLowerCase();
+    }
+
+    if(tipo=="reptil"){
+      contRep++;
+      acumEdadRep += edad;
+
+
+    }else if(tipo=="ave"){
+      contAve++;
+
+
+    }else{// tipo = mamifero
+      contmamifero++;
+
+    }
+
+    contMascotas++;
+
+
+    if(flagMasJov || edad < edadMasJov){ //B
+      edadMasJov= edad;
+      tipoMasJov=tipo;
+      sexoMasJov= sexo;
+      flagMasJov =0;
+    }  
+
+
+    respuesta=prompt("desea ingresar mas animales: s / n" );
+  }while(respuesta =="s")
+
+
+  ////////////////////////////////////////////
+
+  if(contRep!=0){ //A
+    promEdadRep = acumEdadRep / contRep;
+  }
+
+
+  if(contAve!=0){// C
+    procentAves = contMascotas *100 / contAve
+  }
+
+  if(contAve > contRep && contAve > contmamifero){
+    maxCantMascotas = "Aves"
+
+  }else if(contRep >= contAve && contRep > contmamifero){
+    maxCantMascotas = "Reptiles";
+
+  }else{
+    maxCantMascotas = "mamiferos";
+  }
+
+ //////////////////////////////////////////////////////
+ document.write("A) promedio de edad de los reptiles es: " + promEdadRep + "<br>");
+ document.write("B) La mascota mas joven es de tipo " + tipoMasJov + " y tiene el sexo "+ sexoMasJov + "<br>");
+ document.write("C) el procentaje de aves es: " + procentAves +"<br>");
+ document.write("D) la mayor cantidad de mascotas son: " + maxCantMascotas + "<br>" )
+
+
+
 }
